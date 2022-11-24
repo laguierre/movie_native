feaa3d00b994f25e53f9f54089f6ba9eda22ebca
Lean <laguierre@gmail.com>
2022-11-23 22:08:01 -0300

    231122

--- app/src/main/java/com/example/movienative/DetailsMovie.java
+++ app/src/main/java/com/example/movienative/DetailsMovie.java
@@ -2,13 +2,37 @@ package com.example.movienative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.nio.charset.StandardCharsets;
 
 public class DetailsMovie extends AppCompatActivity {
+    private TextView tvMovieTitle;
+    private ImageView ivImage;
+    private TextView tvOriginalTitle;
+    private TextView tvBriefMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_movie);
+
+        tvMovieTitle = (TextView) findViewById(R.id.tvTitleMovie);
+        tvOriginalTitle = (TextView) findViewById(R.id.tvOriginalMovie);
+        tvBriefMovie = (TextView) findViewById(R.id.tvBrief);
+        ivImage = (ImageView) findViewById(R.id.ivPosterMovie);
+
+        Intent intent = getIntent();
+        String titleMovie = intent.getStringExtra(MainActivity.MOVIE_TITLE);
+        String brief = intent.getStringExtra(MainActivity.BRIEF_MOVIE);
+        String originalTitle = intent.getStringExtra(MainActivity.MOVIE_ORIGINAL_TITLE);
+        int urlPoster = Integer.parseInt(intent.getStringExtra(MainActivity.URL_IMAGE));
+        tvMovieTitle.setText(titleMovie);
+        tvOriginalTitle.setText(originalTitle);
+        tvBriefMovie.setText(intent.getStringExtra(MainActivity.URL_IMAGE));
+        ivImage.setImageResource(urlPoster);
     }
 }
--- app/src/main/java/com/example/movienative/MainActivity.java
+++ app/src/main/java/com/example/movienative/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.movienative;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.graphics.Color;
@@ -12,15 +13,27 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MOVIE_TITLE = "movie title";
+    public static final String MOVIE_ORIGINAL_TITLE = "original movie title";
+    public static final String URL_IMAGE = "0";
+    public static final String BRIEF_MOVIE = "brief movie";
+    public static final String AVERAGE_VOTE = "average vote";
+
+
+
 
     private ListView lvMovies;
     private String[] moviesTitle = {"Avatar: El camino del agua", "Black Adam", "Thor: \nAmor y True", "Black Panther: Wakanda Para siempre", "Smile"};
     private String[] moviesOriginalTitle = {"Avatar: The way of water", "Black Adam", "Thor: Love and Thunder", "Black Panther: Wakanda Forever", "Smile"};
     private String[] averageVote = {"8.9", "7.5", "7.9", "7.3", "6.8"};
+    private String[] briefMovie = {"8.9", "7.5", "7.9", "7.3", "6.8"};
 
-    private int[] imageMovies = {0, 0, 0, 0, 0, 0};
+    private int[] imageMovies = {R.drawable.avatar2, R.drawable.blackadam, R.drawable.thor, R.drawable.wakanda, R.drawable.smile};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
         CustomBaseAdapterMovie customBaseAdapterMovie = new CustomBaseAdapterMovie(getApplicationContext(), moviesTitle, averageVote, moviesOriginalTitle, imageMovies);
         lvMovies.setAdapter(customBaseAdapterMovie);
-        int x = getResources().getIdentifier("avatar2", "avatar2/avatar2", getPackageName());
-        Log.i("image", "Image: " + String.valueOf(x));
 
         lvMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -44,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
                 String mensaje = lvMovies.getItemAtPosition(i) + "voto: " + averageVote[i];
                 Log.i("Click", mensaje);
                 Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(view.getContext(), DetailsMovie.class);
+                intent.putExtra(MOVIE_TITLE, moviesTitle[i]);
+                intent.putExtra(MOVIE_ORIGINAL_TITLE, moviesOriginalTitle[i]);
+                intent.putExtra(URL_IMAGE, imageMovies[i]);
+                intent.putExtra(AVERAGE_VOTE, averageVote[i]);
+                intent.putExtra(BRIEF_MOVIE, briefMovie[i]);
+                startActivity(intent);
             }
         });
     }
